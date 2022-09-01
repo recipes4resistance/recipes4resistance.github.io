0ab3a359451e415b67553c27984c01cb76d95efd
Victoria C. Chávez <vcchavez528@gmail.com>
2022-09-01 16:52:14 -0400

    fixed sticky navbar

--- assets/vendor/startbootstrap-clean-blog/js/scripts.js
+++ assets/vendor/startbootstrap-clean-blog/js/scripts.js
@@ -18,29 +18,19 @@
     // Show the navbar when the page is scrolled up
     var MQL = 992;
 
-    //primary navigation slide-in effect
-    if ($(window).width() > MQL) {
-        var headerHeight = $('#mainNav').height();
-        $(window).on('scroll', {
-                previousTop: 0
-            },
-            function () {
-                var currentTop = $(window).scrollTop();
-                //check if user is scrolling up
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
-                        $('#mainNav').addClass('is-visible');
-                    } else {
-                        $('#mainNav').removeClass('is-visible is-fixed');
-                    }
-                } else if (currentTop > this.previousTop) {
-                    //if scrolling down...
-                    $('#mainNav').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
-                }
-                this.previousTop = currentTop;
-            });
+
+
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("mainNav");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
     }
 
 })(jQuery); // End of use strict
